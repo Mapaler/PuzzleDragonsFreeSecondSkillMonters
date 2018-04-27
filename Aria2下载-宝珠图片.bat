540d2add68af23da5307f74304d6dcaaea3b6dc9
枫谷剑仙 <mapaler@163.com>
2018-04-27 15:56:07 +0800

    换掉因为联网失败导致的图片下载错误

--- Aria2下载-宝珠图片.bat
+++ Aria2下载-宝珠图片.bat
@@ -1,4 +1,4 @@
 @echo off
 title 使用Aria2下载宝珠图片
-aria2c.exe --input-file=Aria2下载列表文件-宝珠图片.down --allow-overwrite=true --auto-file-renaming=false --remote-time=true --continue=true
+aria2c.exe --input-file=Aria2下载列表文件-宝珠图片.down --allow-overwrite=false --auto-file-renaming=false --remote-time=true --continue=true
 pause
